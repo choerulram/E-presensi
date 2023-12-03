96892bb4a55abc70b862eec967bd3f2488e8f6d9
Choerul <achmadchoerul12345@gmail.com>
2023-12-03 22:00:31 +0700

    Add dashboar & grafik

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('dosen.dashboard');
 });
+
+// Route::get('/', function () {
+//     return view('layouts.mahasiswa');
+// });
