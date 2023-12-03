9a9979d52c39326c996b9fe2a584ff2f7c3be915
Choerul <achmadchoerul12345@gmail.com>
2023-12-03 23:25:46 +0700

    Add tables

---
+++ database/migrations/2023_12_03_154040_create_students_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string('nim', 9)->unique();
+            $table->string('nama', 50);
+            $table->string('alamat', 255);
+            $table->string('email', 50)->unique();
+            $table->string('no_hp', 15);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+};
---
+++ database/migrations/2023_12_03_161000_create_courses_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->id();
+            $table->string('kode_matkul', 10)->unique();
+            $table->string('nama_matkul', 100);
+            $table->string('semester', 15);
+            $table->string('sks', 10);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('courses');
+    }
+};
